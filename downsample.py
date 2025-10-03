a9d27e53af6143b46deb22b10fb4fe727bca3ee6
TanishqSharma2022 <tanishq22@iiserb.ac.in>
2025-10-03 12:21:15 +0530

    readme updated

--- downsample.py
+++ downsample.py
@@ -13,35 +13,36 @@ parser = argparse.ArgumentParser(prog="ASCIIArtDownsampler",
                                  description="Downsamples ascii arts by an integer factor")
 
 parser.add_argument("ascii", help="add ascii filepath...")
-
+parser.add_argument("-f", "--factor", help="scaling factor (1, 2, 4)...")
 
 args = parser.parse_args()
 # Example usage
 file_path = args.ascii # replace with your file path
 ascii_art = load_ascii_art(file_path)
 
-
-def downsample_ascii(binary_art, new_h, new_w):
+scaling_factor = int(args.factor)
+def downsample_ascii(binary_art):
     H, W = len(binary_art), len(binary_art[0])
-    block_h = new_h
-    block_w = new_w
     new_art = []
     j = W
-    for i in range(0, H, 2):
-        row = ''        # new_art += binary_art[i]
-        for j in range(0, W, 2):
-            
+    for i in range(0, H, scaling_factor):
+        row = ''    
+        for j in range(0, W, scaling_factor):
             row+=binary_art[i][j]
-
         new_art.append(row)            
     return new_art
 
 
-downsample_art = downsample_ascii(ascii_art, 2, 2)
+downsample_art = downsample_ascii(ascii_art)
 
-# Assume downsample_art is a list of strings (each row of ASCII art)
-with open("output.txt", "w") as f:
-    for row in downsample_art:
-        f.write(row + "\n")
+for row in downsample_art:
+    print(row)
+
+
+
+# with open("output.txt", "w") as f:
+#     for row in downsample_art:
+#         f.write(row + "\n")
+
 
 
